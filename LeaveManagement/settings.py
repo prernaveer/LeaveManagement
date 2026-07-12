ffb72427694b8717705879e6cbab05fe1015f355
Prerna Veer <prernaveer2005@gmail.com>
2026-07-12 17:06:21 +0530

    Switched database from SQLite to MySQL

--- LeaveManagement/settings.py
+++ LeaveManagement/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'employee',
+
 ]
 
 MIDDLEWARE = [
@@ -76,9 +77,13 @@ WSGI_APPLICATION = 'LeaveManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leave_management',
+        'USER': 'root',
+        'PASSWORD': 'Root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
 }
 
 
